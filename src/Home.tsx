511fa45e86e06fbaba73cb01418271ef0555e398
CaptainIRS <36656347+CaptainIRS@users.noreply.github.com>
2024-12-07 07:30:32 +0530

    temp

---
+++ src/Home.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button"
+import { Input } from "@/components/ui/input"
+
+
+export default function Home() {
+  return (
+    <div>
+      <Input />
+      <Button>Home</Button>
+    </div>
+  )
+}
